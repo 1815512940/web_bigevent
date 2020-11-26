ec7412e83ad2ae52ef23e52387bc6be39ebccd01
itwwx <1815512940@qq.com>
2020-11-26 10:32:08 +0800

    完成了首页功能

--- assets/js/beseApl.js
+++ assets/js/beseApl.js
@@ -1,5 +1,20 @@
 var baseAPL = 'http://ajax.frontend.itheima.net'
-// 
+// 拦截所有Ajax请求
 $.ajaxPrefilter(function  (params) {
     params.url = baseAPL + params.url
+
+    // 必须以 /my 开头才行
+    if ( params.url.indexOf('/my/') !== -1) {
+        params.headers =  {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 拦截所有响应，判断身份认证信息
+    params.complete = function (res) {
+        console.log(res);
+        if (res.responseJSON.status !== 0 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$(function () {
+    getUserInof()
+
+    var layer = layui.layer
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            //清空本地tokan
+            localStorage.removeItem('token')
+            // 页面跳转到登录页面
+            location.href = '/login.html'
+            // 关闭弹出框
+            layer.close(index);
+          })
+    })
+})
+
+// 获取用户信息（封装到人口函数的外面）
+// 因为后面要用  全局变量
+function getUserInof() {
+    $.ajax({
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status !== 0) return layui.layer.msg(res.message)
+            console.log(res);
+            renderAvater(res.data)
+        }
+    })
+}
+
+function renderAvater(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html(`欢迎  ${name}`)
+
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').show().prop('src', user.user_pic)
+        $('.text-avater').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var text = name[0].toUpperCase()
+        $('.text-avater').show().html(text)
+    }
+}
